1c8dc351c485e1e6dfea13af7f90df5dd3696973
Liam <ltwirth@asu.edu>
2024-08-10 19:17:33 -0700

    Working minimum viable product

--- src/boid.rs
+++ src/boid.rs
@@ -1,27 +1,16 @@
+use crate::Values;
+use crate::BOUNDS;
 use bevy::math::Vec2;
-use bevy::math::*;
 use bevy::prelude::*;
-use bevy::{
-    math::Vec3Swizzles,
-    prelude::*,
-    render::{mesh::*, render_asset::RenderAssetUsages},
-    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
-    tasks::ComputeTaskPool,
-};
-#[derive(Component)]
-struct Boid {
-    velocity: Vec2,
-    position: Vec2,
-    acceleration: Vec2,
-    max_speed: f32,
-    max_force: f32,
-    separation_weight: f32,
-    alignment_weight: f32,
-    cohesion_weight: f32,
-    separation_radius: f32,
-    alignment_radius: f32,
-    cohesion_radius: f32,
-}
+use bevy::sprite::MaterialMesh2dBundle;
+use bevy::sprite::Mesh2dHandle;
+use bevy::tasks::ComputeTaskPool;
+use bevy_spatial::kdtree;
+use bevy_spatial::kdtree::KDTree2;
+use bevy_spatial::SpatialAccess;
+use halton::Sequence;
+use rand::Rng;
+
 #[derive(Component)]
 pub struct Velocity(Vec2);
 
@@ -34,6 +23,9 @@ pub struct BoidBundle {
     velocity: Velocity,
 }
 
+#[derive(Event)]
+pub struct DvEvent(Entity, Vec2);
+
 impl Default for BoidBundle {
     fn default() -> Self {
         Self {
@@ -41,4 +33,221 @@ impl Default for BoidBundle {
             velocity: Velocity(Vec2::default()),
         }
     }
+}
+
+pub fn boid_setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    values: Res<Values>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    let mut rng = rand::thread_rng();
+    let seq = halton::Sequence::new(2)
+        .zip(Sequence::new(3))
+        .zip(1..values.boid_count);
+
+    for ((x, y), _) in seq {
+        let spawn_x = (x as f32 * BOUNDS.x) - BOUNDS.x / 2.0;
+        let spawn_y = (y as f32 * BOUNDS.y) - BOUNDS.y / 2.0;
+        let mut transform =
+            Transform::from_xyz(spawn_x, spawn_y, 0.0).with_scale(Vec3::splat(values.boid_size)); // Fixed typo here
+        transform.rotate_z(0.0);
+
+        let velocity = Velocity(
+            Vec2::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0)) * values.boid_speed,
+        );
+
+        commands.spawn((
+            BoidBundle {
+                mesh: MaterialMesh2dBundle {
+                    mesh: Mesh2dHandle(meshes.add(Circle { radius: 4.0 })),
+                    material: materials.add(Color::hsl(360. * rng.gen::<f32>(), rng.gen(), 0.7)),
+                    transform,
+                    ..default()
+                },
+                velocity,
+            },
+            SpatialEntity,
+        ));
+    }
+}
+
+fn get_dv(
+    kdtree: &Res<KDTree2<SpatialEntity>>,
+    boid_query: &Query<(Entity, &Velocity, &Transform), With<SpatialEntity>>,
+    camera: &Query<(&Camera, &GlobalTransform)>,
+    window: &Query<&Window>,
+    boid: &Entity,
+    t0: &&Transform,
+    values: &Res<Values>,
+) -> Vec2 {
+    // https://vanhunteradams.com/Pico/Animal_Movement/Boids-algorithm.html
+    let mut dv = Vec2::default();
+    let mut vec_away = Vec2::default();
+    let mut avg_position = Vec2::default();
+    let mut avg_velocity = Vec2::default();
+    let mut neighboring_boids = 0;
+    let mut close_boids = 0;
+
+    for (_, entity) in kdtree.k_nearest_neighbour(t0.translation.xy(), values.max_neighbors) {
+        let Ok((other, v1, t1)) = boid_query.get(entity.unwrap()) else {
+            todo!()
+        };
+
+        // Don't evaluate against itself
+        if *boid == other {
+            continue;
+        }
+
+        let vec_to = (t1.translation - t0.translation).xy();
+        let dist_sq = vec_to.x * vec_to.x + vec_to.y * vec_to.y;
+
+        // Don't evaluate boids out of range
+        if dist_sq > values.vis_range_sq {
+            continue;
+        }
+
+        // Don't evaluate boids behind
+        if let Some(vec_to_norm) = vec_to.try_normalize() {
+            if t0
+                .rotation
+                .angle_between(Quat::from_rotation_arc_2d(Vec2::X, vec_to_norm))
+                > values.boid_fov
+            {
+                continue;
+            }
+        }
+
+        if dist_sq < values.prot_range_sq {
+            // separation
+            vec_away -= vec_to;
+            close_boids += 1;
+        } else {
+            // cohesion
+            avg_position += vec_to;
+            // alignment
+            avg_velocity += v1.0;
+            neighboring_boids += 1;
+        }
+    }
+
+    if neighboring_boids > 0 {
+        let neighbors = neighboring_boids as f32;
+        dv += avg_position / neighbors * values.boid_centering_factor;
+        dv += avg_velocity / neighbors * values.boid_matching_factor;
+    }
+
+    if close_boids > 0 {
+        let close = close_boids as f32;
+        dv += vec_away / close * values.boid_avoidance_factor;
+    }
+
+    // Chase the mouse
+    let (camera, t_camera) = camera.single();
+    if let Some(c_window) = window.single().cursor_position() {
+        if let Some(c_world) = camera.viewport_to_world_2d(t_camera, c_window) {
+            let to_cursor = c_world - t0.translation.xy();
+            dv += to_cursor * values.boid_mouse_chase_factor;
+        };
+    };
+
+    dv
+}
+
+pub fn flocking_system(
+    boid_query: Query<(Entity, &Velocity, &Transform), With<SpatialEntity>>,
+    kdtree: Res<KDTree2<SpatialEntity>>,
+    mut dv_event_writer: EventWriter<DvEvent>,
+    camera: Query<(&Camera, &GlobalTransform)>,
+    window: Query<&Window>,
+    values: Res<Values>,
+) {
+    let pool = ComputeTaskPool::get();
+    let boids = boid_query.iter().collect::<Vec<_>>();
+    let boids_per_thread = (boids.len() + pool.thread_num() - 1) / pool.thread_num();
+
+    // https://docs.rs/bevy/latest/bevy/tasks/struct.ComputeTaskPool.html
+    // https://github.com/kvietcong/rusty-boids
+    for batch in pool.scope(|s| {
+        for chunk in boids.chunks(boids_per_thread) {
+            let kdtree = &kdtree;
+            let boid_query = &boid_query;
+            let camera = &camera;
+            let window = &window;
+            let values = &values;
+
+            s.spawn(async move {
+                let mut dv_batch: Vec<DvEvent> = vec![];
+
+                for (boid, _, t0) in chunk {
+                    dv_batch.push(DvEvent(
+                        *boid,
+                        get_dv(kdtree, boid_query, camera, window, boid, t0, values),
+                    ));
+                }
+
+                dv_batch
+            });
+        }
+    }) {
+        dv_event_writer.send_batch(batch);
+    }
+}
+
+pub fn velo_system(
+    mut events: EventReader<DvEvent>,
+    mut boids: Query<(&mut Velocity, &mut Transform)>,
+    window: Query<&Window>,
+    values: Res<Values>,
+) {
+    for DvEvent(boid, dv) in events.read() {
+        let Ok((mut velocity, transform)) = boids.get_mut(*boid) else {
+            todo!()
+        };
+
+        velocity.0.x += dv.x;
+        velocity.0.y += dv.y;
+
+        let res = &window.single().resolution;
+
+        let width = (res.width() - values.boid_bound_size) / 2.;
+        let height = (res.height() - values.boid_bound_size) / 2.;
+
+        // Steer back into visible region
+        if transform.translation.x < -width {
+            velocity.0.x += values.boid_turn_factor;
+        }
+        if transform.translation.x > width {
+            velocity.0.x -= values.boid_turn_factor;
+        }
+        if transform.translation.y < -height {
+            velocity.0.y += values.boid_turn_factor;
+        }
+        if transform.translation.y > height {
+            velocity.0.y -= values.boid_turn_factor;
+        }
+
+        // Clamp speed
+        let speed = velocity.0.length();
+
+        if speed < values.boid_min_speed {
+            velocity.0 *= values.boid_min_speed / speed;
+        }
+        if speed > values.boid_max_speed {
+            velocity.0 *= values.boid_max_speed / speed;
+        }
+    }
+}
+pub fn movement_system(mut query: Query<(&mut Velocity, &mut Transform)>) {
+    for (velocity, mut transform) in query.iter_mut() {
+        transform.rotation = Quat::from_axis_angle(Vec3::Z, steer_to(Vec2::ZERO, velocity.0));
+        transform.translation.x += velocity.0.x;
+        transform.translation.y += velocity.0.y;
+    }
+}
+fn steer_to(a: Vec2, b: Vec2) -> f32 {
+    // https://stackoverflow.com/a/68929139
+    let dir = b - a;
+    dir.y.atan2(dir.x)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 mod boid;
 
 use bevy::{
@@ -6,6 +5,7 @@ use bevy::{
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
+use std::sync::Arc;
 use bevy_spatial::{kdtree::KDTree2, SpatialAccess};
 use bevy_spatial::{AutomaticUpdate, SpatialStructure};
 use boid::*;
@@ -14,42 +14,95 @@ use rand::prelude::*;
 use std::time::Duration;
 pub const CLEAR: Color = Color::srgb(0.0, 0.0, 0.0);
 
-pub struct nums {
+pub const BOUNDS: Vec2 = Vec2::new(800.0, 400.0);
+
+use boid::SpatialEntity;
+use boid::*;
+
+#[derive(Resource, Copy, Clone)]
+pub struct Values {
+    /// Number of boids to spawn
     boid_count: i32,
+    /// Size of the boids
+    boid_size: f32,
+    /// Speed of the boids
     boid_speed: f32,
+    ///Maximum number of neighbors a boid can have
+    max_neighbors: usize,
+    /// Visibility range of the boids, determines how far away a boid can see another boid
     boid_vis_range: f32,
+    /// Protection range of the boids, determines how far away a boid can see a predator/obstacle
+    /// or a boid that is determined to be "too close"
     boid_prot_range: f32,
+    /// Factor/amount that the boids want to center around the center of mass of the boids
     boid_centering_factor: f32,
+    /// Factor/amount that the boids want to avoid each other
     boid_avoidance_factor: f32,
+    /// Factor/amount that the boids want to match the velocity of the boids around them
     boid_matching_factor: f32,
-
+    /// Minimum speed of the boids
     boid_min_speed: f32,
+    /// Maximum speed of the boids
     boid_max_speed: f32,
+    ///boid field of view
+    boid_fov: f32,
 
+    vis_range_sq: f32,
+    prot_range_sq: f32,
+
+    boid_mouse_chase_factor: f32,
+    boid_bound_size: f32,
+    boid_turn_factor: f32,
+
+    is_toroidal: bool,
 }
+
+impl Default for Values {
+    fn default() -> Self {
+        Self {
+            boid_count: 1000,
+            boid_size: 0.27,
+            boid_speed: 5.,
+            max_neighbors: 100,
+            boid_vis_range: 35.0, 
+            boid_fov: 120.0 * std::f32::consts::PI / 180.0,
+            boid_bound_size: 98.0,
+            boid_turn_factor: 0.5,
+            boid_prot_range: 10.0,
+            boid_centering_factor: 0.0008,
+            boid_mouse_chase_factor: 0.0006,
+            boid_avoidance_factor: 0.05,
+            boid_matching_factor: 0.05,
+            boid_min_speed: 5.,
+            boid_max_speed: 10.,
+
+            vis_range_sq: 35.0 * 35.0,
+            prot_range_sq: 10.0 * 10.0,
+            is_toroidal: true,
+        }
+    }
+}
+
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        // Track boids in the KD-Tree
-        .add_plugins(
-            AutomaticUpdate::<SpatialEntity>::new()
-                // TODO: check perf of other tree types
-                .with_spatial_ds(SpatialStructure::KDTree2)
-                .with_frequency(Duration::from_millis(16)),
-        )
+    (App::new()
+            .add_plugins((
+                DefaultPlugins.set(WindowPlugin {
+                    primary_window: Some(Window {
+                        canvas: Some("#bevy_boids_canvas".into()),
+                        resizable: true,
+                        ..default()
+                    }),
+                    ..default()
+                }),
+                // Track boids in the KD-Tree
+                AutomaticUpdate::<SpatialEntity>::new()
+                    .with_spatial_ds(SpatialStructure::KDTree2)
+                    .with_frequency(Duration::from_millis(16)),
+            )))
+        .insert_resource(Values::default())
         .insert_resource(Time::<Fixed>::from_hz(60.0))
-        .add_systems(Startup, setup)
+        .add_event::<DvEvent>()
+        .add_systems(Startup, boid_setup)
+        .add_systems(FixedUpdate, (velo_system, movement_system, flocking_system))
         .run();
-}
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
- commands.spawn(Camera2dBundle::default());
-
-    let mut rng = rand::thread_rng();
-
-    let seq = halton::Sequence::new(2).zip(Sequence::new(3)).zip(1..BOID_COUNT)
 }
